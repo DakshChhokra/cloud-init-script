ea06723d3d57a9057a95f0e3aa3a9484a83eeca4
Daksh Chhokra <chhokradaksh+github@gmail.com>
2021-08-13 12:43:31 -0500

    v3

--- cloud-init-v3.sh
+++ cloud-init-v3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # these Keep Failing
-sudo apt update -y 
-sudo apt upgrade -y
+# sudo apt update -y 
+# sudo apt upgrade -y
 
 # jq
 sudo apt install jq -y
@@ -26,6 +26,8 @@ echo "alias k=kubectl" | sudo tee /home/adminuser/.bash_aliases
 
 #get github
 sudo apt install git -y
+
+#github ssh setup
 ssh-keygen -t ed25519 -C "dchhokra@thebhwgroup.com" -N aks -f ~/.ssh/id_ed25519 -q
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
