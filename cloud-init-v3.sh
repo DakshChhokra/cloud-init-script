4b0234b12f5a90bf909b453c301cbcd62d058ecd
Daksh Chhokra <chhokradaksh+github@gmail.com>
2021-08-13 17:07:28 -0500

    v3

--- cloud-init-v3.sh
+++ cloud-init-v3.sh
@@ -25,7 +25,7 @@ echo "alias k=kubectl" | sudo tee /home/adminuser/.bash_aliases
 
 
 #get github
-sudo apt installgit -y
+sudo apt install git -y
 
 #github ssh setup
 # ssh-keygen -t ed25519 -C "dchhokra@thebhwgroup.com" -N aks -f ~/.ssh/id_ed25519 -q
