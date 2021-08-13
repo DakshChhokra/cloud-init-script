925577f54540101f6564ea83aa6abd519b271ea5
Daksh Chhokra <chhokradaksh+github@gmail.com>
2021-08-13 11:10:08 -0500

    v3

---
+++ cloud-init-v3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# these Keep Failing
+# sudo apt update -y 
+# sudo apt upgrade -y
+
+# jq
+sudo apt install jq -y
+
+# unzip
+sudo apt install unzip -y
+
+# azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# helm
+curl -sL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | sudo bash
+
+# kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+echo "alias k=kubectl" | sudo tee /home/adminuser/.bash_aliases
+
+
+#get github
+sudo apt install git -y
+ssh-keygen -t ed25519 -C "dchhokra@thebhwgroup.com" -N aks -f ~/.ssh/id_ed25519 -q
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "PUBLIC KEY"
+cat ~/.ssh/id_ed25519.pub
